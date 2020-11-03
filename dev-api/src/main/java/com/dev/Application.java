dadc4ae71f3f3ba3350284bb801b233827a7d491
wangning136876 <wangning001>
2020-11-03 17:29:36 +0800

    register

--- dev-api/src/main/java/com/dev/Application.java
+++ dev-api/src/main/java/com/dev/Application.java
@@ -1,16 +1,16 @@
 package com.dev;
 
-;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
+
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.dev.mapper")
+@EnableTransactionManagement
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
-
 }
