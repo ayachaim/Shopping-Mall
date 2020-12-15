487436387297e5a19be1a7bb539d18a586072644
wangning136876 <376885153@qq.com>
2020-12-15 23:18:30 +0800

    商品具体评论信息

--- dev-pojo/src/main/java/com/dev/pojo/vo/ItemCommentVO.java
+++ dev-pojo/src/main/java/com/dev/pojo/vo/ItemCommentVO.java
@@ -1,8 +1,61 @@
 package com.dev.pojo.vo;
 
 /**
- * 用于展示商品评价等级的VO
+ * 查询某种商品的评论信息
  */
 public class ItemCommentVO {
+    private Integer commentLevel;
+    private String content;
+    private String specName;
+    private String createdTime;
+    private String userFace;
+    private String nickName;
 
+    public Integer getCommentLevel() {
+        return commentLevel;
+    }
+
+    public void setCommentLevel(Integer commentLevel) {
+        this.commentLevel = commentLevel;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSpecName() {
+        return specName;
+    }
+
+    public void setSpecName(String specName) {
+        this.specName = specName;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getUserFace() {
+        return userFace;
+    }
+
+    public void setUserFace(String userFace) {
+        this.userFace = userFace;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
 }
