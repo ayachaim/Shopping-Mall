52af588bb7fce7f468822dd08fd6a82ae528be72
wangning <376885153@qq.com>
2020-12-21 21:43:34 +0800

    三级分类标签点击搜索

--- dev-mapper/src/main/java/com/dev/mapper/ItemsMapperCustom.java
+++ dev-mapper/src/main/java/com/dev/mapper/ItemsMapperCustom.java
@@ -1,6 +1,7 @@
 package com.dev.mapper;
 
 import com.dev.pojo.vo.ItemCommentVO;
+import com.dev.pojo.vo.SearchItemsByThirdCatVO;
 import com.dev.pojo.vo.SearchItemsVO;
 import org.apache.ibatis.annotations.Param;
 
@@ -10,5 +11,5 @@ import java.util.Map;
 public interface ItemsMapperCustom  {
     public List<ItemCommentVO> queryItemComments(@Param("paramsMap") Map<String,Object> map);
     public List<SearchItemsVO> searchItemsSpec(@Param("paramsMap") Map<String,Object> map);
-    public List<SearchItemsVO> searchItemsByThirdCat(@Param("paramsMap") Map<String,Object> map);
+    public List<SearchItemsByThirdCatVO> searchItemsByThirdCat(@Param("paramsMap") Map<String,Object> map);
 }
---
+++ dev-pojo/src/main/java/com/dev/pojo/vo/SearchItemsByThirdCatVO.java
@@ -0,0 +1,52 @@
+package com.dev.pojo.vo;
+
+/**
+ * 搜索查询商品
+ */
+public class SearchItemsByThirdCatVO {
+    private Integer itemId;
+    private String ItemName;
+    private Integer sellCounts;
+    private String imgUrl;
+    private String minPrice;
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return ItemName;
+    }
+
+    public void setItemName(String itemName) {
+        ItemName = itemName;
+    }
+
+    public Integer getSellCounts() {
+        return sellCounts;
+    }
+
+    public void setSellCounts(Integer sellCounts) {
+        this.sellCounts = sellCounts;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(String minPrice) {
+        this.minPrice = minPrice;
+    }
+}
