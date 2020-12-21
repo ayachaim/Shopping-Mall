de458624be60639741b59da06ec87cb55e24ea05
wangning <376885153@qq.com>
2020-12-21 21:45:33 +0800

    三级分类标签点击搜索

--- dev-mapper/src/main/java/com/dev/mapper/ItemsMapperCustom.java
+++ dev-mapper/src/main/java/com/dev/mapper/ItemsMapperCustom.java
@@ -1,7 +1,6 @@
 package com.dev.mapper;
 
 import com.dev.pojo.vo.ItemCommentVO;
-import com.dev.pojo.vo.SearchItemsByThirdCatVO;
 import com.dev.pojo.vo.SearchItemsVO;
 import org.apache.ibatis.annotations.Param;
 
@@ -11,5 +10,11 @@ import java.util.Map;
 public interface ItemsMapperCustom  {
     public List<ItemCommentVO> queryItemComments(@Param("paramsMap") Map<String,Object> map);
     public List<SearchItemsVO> searchItemsSpec(@Param("paramsMap") Map<String,Object> map);
-    public List<SearchItemsByThirdCatVO> searchItemsByThirdCat(@Param("paramsMap") Map<String,Object> map);
+
+    /**
+     * 根据三级标签点击查询商品列表
+     * @param map
+     * @return VO
+     */
+    public List<SearchItemsVO> searchItemsByThirdCat(@Param("paramsMap") Map<String,Object> map);
 }
