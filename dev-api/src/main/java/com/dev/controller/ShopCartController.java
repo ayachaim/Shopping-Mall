5a6e55e76cc1859b1120e866447c33c8a0121419
wangning <376885153@qq.com>
2020-12-24 21:55:58 +0800

    购物车VO

--- dev-api/src/main/java/com/dev/controller/ShopCartController.java
+++ dev-api/src/main/java/com/dev/controller/ShopCartController.java
@@ -2,18 +2,27 @@ package com.dev.controller;
 
 import com.dev.Utils.JSONResult;
 import com.dev.pojo.bo.ShopCartBo;
+import com.dev.pojo.vo.ShopCartVO;
+import com.dev.service.ItemService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Api(value = "购物车",tags = {"购物车数据"})
 @RestController
 @RequestMapping("shopcart")
 public class ShopCartController {
+
+    @Autowired
+    private ItemService itemsService;
+
     @ApiOperation(value = "添加购物车",notes = "添加购物车",httpMethod = "POST")
     @PostMapping("/add")
     public JSONResult add(
@@ -28,6 +37,20 @@ public class ShopCartController {
         System.out.println(shopCartBo);
         //TODO redis
         return JSONResult.ok();
+    }
+
+
+    @ApiOperation(value = "根据商品规格id查询最新的商品",notes = "根据商品规格id查询最新的商品",httpMethod = "GET")
+    @GetMapping("/refresh")
+    public JSONResult refresh(
+            @ApiParam(name = "specIds" , value = "字符串拼接规格ids" , required = true , example = "1,2,3")
+            @RequestParam String specIds
+    ) {
+        if(StringUtils.isBlank(specIds)){
+            return JSONResult.ok();
+        }
+        List<ShopCartVO> list = itemsService.queryItemsBySpecIds(specIds);
+        return JSONResult.ok(list);
     }
 
 }
