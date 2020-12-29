3e5c7f1e133c0b3802d8a39f898028b1f7ffc983
wangning <376885153@qq.com>
2020-12-29 21:35:49 +0800

    订单BO

--- dev-service/src/main/java/com/dev/service/impl/OrderServiceImpl.java
+++ dev-service/src/main/java/com/dev/service/impl/OrderServiceImpl.java
@@ -1,8 +1,10 @@
 package com.dev.service.impl;
 
 import com.dev.mapper.OrdersMapper;
+import com.dev.pojo.Orders;
 import com.dev.pojo.bo.OrderBO;
 import com.dev.service.OrderService;
+import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -13,8 +15,8 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private OrdersMapper ordersMapper;
-
-
+    @Autowired
+    private Sid sid;
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public void createOrder(OrderBO orderBO) {
@@ -23,6 +25,14 @@ public class OrderServiceImpl implements OrderService {
         String itemSpec = orderBO.getItemSpecIds();
         Integer payMethod = orderBO.getPayMethod();
         String leftMsg = orderBO.getLeftMsg();
-
+        //邮费设置为0
+        Integer postAmount = 0;
+        //生成16位订单id
+        String orderId = sid.nextShort();
+        Orders newOrder = new Orders();
+        //设置订单id
+        newOrder.setId(orderId);
+        newOrder.setUserId(userId);
+        //TODO 其他的db字段都要设置进去
     }
 }
