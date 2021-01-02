e0f10b7858284b912c1841bc2daf77efe77a5d1e
wangning <376885153@qq.com>
2021-01-02 19:40:07 +0800

    设置订单

--- dev-service/src/main/java/com/dev/service/impl/OrderServiceImpl.java
+++ dev-service/src/main/java/com/dev/service/impl/OrderServiceImpl.java
@@ -24,7 +24,6 @@ public class OrderServiceImpl implements OrderService {
     private AddressService addressService;
     @Autowired
     private OrdersMapper ordersMapper;
-
     @Autowired
     private OrderItemsMapper orderItemsMapper;
     @Autowired
